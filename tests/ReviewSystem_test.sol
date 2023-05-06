7c0710043674336b40375763f3b285e9430976fb
trincaw <nikxtr@gmail.com>
2023-05-06 20:14:44 +0200

    prova

--- tests/ReviewSystem_test.sol
+++ tests/ReviewSystem_test.sol
@@ -1,58 +1,44 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "remix_tests.sol";
-import "remix_accounts.sol";
 import "../contracts/ReviewSystem.sol";
-import "../contracts/ReviewLibrary.sol";
-import "../contracts/TransactionLibrary.sol";
+import "contracts/ReviewLibrary.sol";
+import "remix_accounts.sol";
 import "hardhat/console.sol";
 
-// SPDX-License-Identifier: MIT
-
 contract TestReviewSystem {
-    ReviewSystem reviewSystem;
+    ReviewSystem reviewSystem = new ReviewSystem();
     address acc0; //owner by default
-    address acc1;
-    address acc2;
     address recipient;
 
     function beforeAll() public {
         acc0 = TestsAccounts.getAccount(0);
-
-        acc1 = TestsAccounts.getAccount(1);
-        acc2 = TestsAccounts.getAccount(2);
-        recipient = TestsAccounts.getAccount(3);
+        recipient = TestsAccounts.getAccount(1);
     }
 
     /// #value: 1000000000000000000 1 Eth
     /// #sender: account-1
-
     function testSendTransaction() public payable {
         uint recipientOld = recipient.balance;
         Assert.equal(msg.value, 1000000000000000000, "value should be 1 Eth");
-        reviewSystem.sendTransaction{value: 100 wei}(recipient);
-        Assert.ok(
-            recipientOld < recipient.balance,
-            "increased transaction number"
-        );
+        reviewSystem.sendTransaction{value: 1 ether}(recipient);
+        Assert.ok(recipientOld < recipient.balance, "asd");
     }
 
     function testAddReview() public {
         console.log(0);
-        reviewSystem.sendTransaction{value: 100 wei}(acc2);
+        reviewSystem.sendTransaction{value: 100 wei}(recipient);
 
-        uint lenghtOld = reviewSystem.getReviewsByReceiver(acc2).length;
+        uint lenghtOld = reviewSystem.getReviewsByReceiver(recipient).length;
 
         TransactionLibrary.Transaction[]
             memory unreviewedTransactions = reviewSystem
-                .getUnreviewedTransactions(acc2);
+                .getUnreviewedTransactions(acc0);
 
-        bytes32 id = unreviewedTransactions[0].id;
-        uint8 rate = 2;
+        reviewSystem.addReview(unreviewedTransactions[0].id, "", 1, "");
 
-        reviewSystem.addReview(id, "", rate, "");
+        uint lenghtNew = reviewSystem.getReviewsByReceiver(recipient).length;
 
-        uint lenghtnew = reviewSystem.getReviewsByReceiver(acc2).length;
-
-        Assert.ok(lenghtOld < lenghtnew, "increased review number");
+        Assert.ok(lenghtOld < lenghtNew, "increased review number");
     }
 }
