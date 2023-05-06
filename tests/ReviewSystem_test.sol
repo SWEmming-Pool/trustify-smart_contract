f5c8469b024b6922809815f132f37d2652387edb
trincaw <nikxtr@gmail.com>
2023-05-06 19:57:36 +0200

    fix

--- tests/ReviewSystem_test.sol
+++ tests/ReviewSystem_test.sol
@@ -1,41 +1,58 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "remix_tests.sol";
-import "../contracts/ReviewSystem.sol";
-import "contracts/ReviewLibrary.sol";
 import "remix_accounts.sol";
+import "../contracts/ReviewSystem.sol";
+import "../contracts/ReviewLibrary.sol";
+import "../contracts/TransactionLibrary.sol";
+import "hardhat/console.sol";
+
+// SPDX-License-Identifier: MIT
 
 contract TestReviewSystem {
-    ReviewSystem reviewSystem = new ReviewSystem();
+    ReviewSystem reviewSystem;
     address acc0; //owner by default
+    address acc1;
+    address acc2;
     address recipient;
 
     function beforeAll() public {
         acc0 = TestsAccounts.getAccount(0);
-        recipient = TestsAccounts.getAccount(1);
+
+        acc1 = TestsAccounts.getAccount(1);
+        acc2 = TestsAccounts.getAccount(2);
+        recipient = TestsAccounts.getAccount(3);
     }
 
     /// #value: 1000000000000000000 1 Eth
     /// #sender: account-1
+
     function testSendTransaction() public payable {
         uint recipientOld = recipient.balance;
         Assert.equal(msg.value, 1000000000000000000, "value should be 1 Eth");
-        reviewSystem.sendTransaction{value: 1 ether}(recipient);
-        Assert.ok(recipientOld < recipient.balance, "asd");
+        reviewSystem.sendTransaction{value: 100 wei}(recipient);
+        Assert.ok(
+            recipientOld < recipient.balance,
+            "increased transaction number"
+        );
     }
 
     function testAddReview() public {
-        ReviewLibrary.Review[] memory rold = reviewSystem.getReviewsForSender(
-            msg.sender
-        );
-        bytes32 transactionId = "0x1235312";
-        string memory title = "Test Review";
-        uint8 rating = 4;
-        string memory text = "This is a test review.";
-        reviewSystem.addReview(transactionId, title, rating, text);
-        ReviewLibrary.Review[] memory rnew = reviewSystem.getReviewsForSender(
-            msg.sender
-        );
-        Assert.equal(rold.length + 1, rnew.length, "Review not added");
+        console.log(0);
+        reviewSystem.sendTransaction{value: 100 wei}(acc2);
+
+        uint lenghtOld = reviewSystem.getReviewsByReceiver(acc2).length;
+
+        TransactionLibrary.Transaction[]
+            memory unreviewedTransactions = reviewSystem
+                .getUnreviewedTransactions(acc2);
+
+        bytes32 id = unreviewedTransactions[0].id;
+        uint8 rate = 2;
+
+        reviewSystem.addReview(id, "", rate, "");
+
+        uint lenghtnew = reviewSystem.getReviewsByReceiver(acc2).length;
+
+        Assert.ok(lenghtOld < lenghtnew, "increased review number");
     }
 }
