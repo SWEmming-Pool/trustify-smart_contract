babda47f03ccba8ca9c7e9fa66ddc2b5226997a5
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2023-05-11 16:05:45 +0200

    Due test

--- tests/ReviewSystem_test.sol
+++ tests/ReviewSystem_test.sol
@@ -1,44 +1,50 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "remix_tests.sol";
-import "../contracts/ReviewSystem.sol";
-import "contracts/ReviewLibrary.sol";
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
+
+// This import is automatically injected by Remix
+import "remix_tests.sol"; 
+
+// This import is required to use custom transaction context
+// Although it may fail compilation in 'Solidity Compiler' plugin
+// But it will work fine in 'Solidity Unit Testing' plugin
 import "remix_accounts.sol";
-import "hardhat/console.sol";
+import "../contracts/ReviewSystem.sol";
 
-contract TestReviewSystem {
-    ReviewSystem reviewSystem = new ReviewSystem();
-    address acc0; //owner by default
-    address recipient;
+// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
+contract testSuite {
 
+    ReviewSystem reviewSystem;
+    address owner; // By default linked to account(0)
+    address sender; // Account 1
+    address receiver; // Account 2
+
+    /// 'beforeAll' runs before all other tests
+    /// More special functions are: 'beforeEach', 'beforeAll', 'afterEach' & 'afterAll'
     function beforeAll() public {
-        acc0 = TestsAccounts.getAccount(0);
-        recipient = TestsAccounts.getAccount(1);
+        reviewSystem = new ReviewSystem();
+        owner = TestsAccounts.getAccount(0);
+        sender = TestsAccounts.getAccount(1);
+        receiver = TestsAccounts.getAccount(2);
     }
 
-    /// #value: 1000000000000000000 1 Eth
+    /// #value: 1000000000000000000 => 1 ether
+    function checkAddress() public payable {
+        Assert.equal(msg.value, 1 ether, "Value should be 1ETH");
+        Assert.equal(owner, TestsAccounts.getAccount(0), "Invalid owner");
+        Assert.equal(sender, TestsAccounts.getAccount(1), "Invalid sender");
+        Assert.equal(receiver, TestsAccounts.getAccount(2), "Invalid receiver");
+    }
+
     /// #sender: account-1
-    function testSendTransaction() public payable {
-        uint recipientOld = recipient.balance;
-        Assert.equal(msg.value, 1000000000000000000, "value should be 1 Eth");
-        reviewSystem.sendTransaction{value: 1 ether}(recipient);
-        Assert.ok(recipientOld < recipient.balance, "asd");
+    /// #value: 1000000000000000000 => 1 ether 
+    function checkSendTransactionOnValidSenderAndValue() public payable {
+        uint receiverOldBalance = receiver.balance;
+        Assert.equal(msg.sender, sender, "Sender should be account(1)");
+        Assert.equal(msg.value, 1 ether, "Value should be 1ETH");
+        reviewSystem.sendTransaction{value: 1 ether}(receiver);
+        Assert.ok(receiver.balance > receiverOldBalance, "Receiver did not receive the fund");
     }
 
-    function testAddReview() public {
-        console.log(0);
-        reviewSystem.sendTransaction{value: 100 wei}(recipient);
-
-        uint lenghtOld = reviewSystem.getReviewsByReceiver(recipient).length;
-
-        TransactionLibrary.Transaction[]
-            memory unreviewedTransactions = reviewSystem
-                .getUnreviewedTransactions(acc0);
-
-        reviewSystem.addReview(unreviewedTransactions[0].id, "", 1, "");
-
-        uint lenghtNew = reviewSystem.getReviewsByReceiver(recipient).length;
-
-        Assert.ok(lenghtOld < lenghtNew, "increased review number");
-    }
 }
+    
